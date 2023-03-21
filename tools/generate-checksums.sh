dd9f2f30bb58962904ec7a2987e9d1a0e5b828f9
Mohammed Naser <mnaser@vexxhost.com>
2023-03-21 18:37:38 +0000

    feat: support multiple k8s versions

---
+++ tools/generate-checksums.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+BINS=(
+  kubeadm
+  kubectl
+  kubelet
+)
+ARCHS=(
+  amd64
+  arm64
+)
+VERSIONS=(
+  1.19.16
+  1.20.15
+  1.21.14
+  1.22.17
+  1.23.17
+  1.24.12
+  1.25.8
+  1.26.3
+)
+
+for b in "${BINS[@]}"; do
+  echo "${b}_checksums:";
+  for a in "${ARCHS[@]}"; do
+    echo "  ${a}:";
+    for i in "${VERSIONS[@]}"; do
+      echo "    ${i}: $(curl -sL https://dl.k8s.io/v${i}/bin/linux/${a}/${b}.sha256)";
+    done;
+  done;
+done;
