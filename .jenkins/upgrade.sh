274cd96b70e6ab9f50355001d6c3c4f6ef6538e7
ricolin <rlin@vexxhost.com>
2024-03-22 00:31:05 +0800

    correct upgrade script

--- .jenkins/upgrade.sh
+++ .jenkins/upgrade.sh
@@ -1,10 +1,27 @@
 #! /bin/bash
 
+set -xe
+
+sudo apt update
 sudo apt-get install -y git python3-pip docker.io
-sudo groupadd docker
+
+# Config Docker
+groups
 sudo usermod -aG docker $USER
+if [ -d "/home/$USER/.docker" ]
+  then
+    sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+    sudo chmod g+rwx "$HOME/.docker" -R
+fi
+sudo systemctl enable docker
 sudo systemctl restart docker
-sudo pip install -r requirements.txt
+sudo chmod 777 /var/run/docker.sock
+#newgrp docker
+docker -v
+docker ps
+
+pip install -r requirements.txt
 sudo swapoff -a
-sudo sysctl fs.inotify.max_user_watches=524288
-molecule test -s cluster-upgrade
+sudo sysctl fs.inotify.max_user_instances=1280
+sudo sysctl fs.inotify.max_user_watches=655360
+PATH=$PATH:$HOME/.local/bin:$HOME/bin && molecule test -s cluster-upgrade
